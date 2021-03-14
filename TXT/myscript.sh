33c51de30e879cfcc3d2d52a65505d023118522f
Johanes Jason <johanesjason88@gmail.com>
2021-03-14 21:20:30 +0700

    edit script

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,13 +1,3 @@
-# Copyright (C) 2020-2021 Cicak Bin Kadal
-# https://www.youtube.com/watch?v=KAXK07ni9gU
-
-# This free document is distributed in the hope that it will be 
-# useful, but WITHOUT ANY WARRANTY; without even the implied 
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# REV02 Fri 12 Mar 13:40:58 WIB 2021
-# REV01 Tue 13 Oct 10:37:14 WIB 2020
-# START Mon 28 Sep 21:05:04 WIB 2020
-
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
